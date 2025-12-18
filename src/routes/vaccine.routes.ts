115ab51b342b7d25dd7cc63033fc3d221316c370
Victoria Fonteny <victoriafonteny@live.fr>
2025-12-18 16:34:57 +0100

    get and get by id vaccines

--- src/routes/vaccine.routes.ts
+++ src/routes/vaccine.routes.ts
@@ -4,14 +4,7 @@ import { Request, Response } from "express";
 
 const router = Router();
 
-console.info('cc')
-
-
-export const updateAnimalController = async (req: Request, res: Response) => {
-res.send('ok' );
-}
-
-router.get("/", updateAnimalController);
+router.get("/", getVaccines);
 router.get("/:id", getByIdVaccine);
 // router.post("/", createVaccineController);
 // router.put("/:id", updateVaccineController);
